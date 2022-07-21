fd422b97a50b155009661c5e0bc6ce309d32ceb0
johannpereze <sebas.jsp@gmail.com>
2022-07-20 20:51:22 -0500

    fix: added global to vite.config to prevent error

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: {},
+  },
 });
+
+/* "define" prevents
+ * Uncaught ReferenceError: global is not defined
+ */

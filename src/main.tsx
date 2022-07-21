4a025c40659859bd4ac696d448d01cb5d3e36223
johannpereze <sebas.jsp@gmail.com>
2022-07-20 20:51:58 -0500

    refactor: moved HOCs from main to App

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./app/store";
-import Counter from "./components/counter/Counter";
+import App from "./App";
+import "./i18n/i18n";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Counter />
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
